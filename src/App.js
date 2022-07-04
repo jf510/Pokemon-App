8422586fe6d68076da30f6c5e1155355e51c957f
Jfontaine <jfontaine19@gmail.com>
2022-07-04 14:35:57 -0400

    Created and set the state for the pagination. Added a cancel token for the requests

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import PokeList from "./PokeList";
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [currentPage, setCurrentPage] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPage, setNextPage] = useState();
+  const [peviousPage, setPreviousPage] = useState()
+  const [loading, setLoading] = useState(true);
   
   useEffect(() => {
-    axios.get(currentPage).then(res => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPage, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false);
       console.log(res.data.results);
       console.log(res.data.results.map(p => p.name));
       setPokemon(res.data.results.map(p => p.name));
+      setNextPage(res.data.next)
+      setPreviousPage(res.data.previous);
     })
+
+    return () => {
+
+    }
   }, [currentPage])
+
+  if (loading) return "Loading..."
 
  
 
