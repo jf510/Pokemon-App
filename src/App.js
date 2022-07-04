2d5f1e0279d74ef7f60a41b962ae6ce4dacbf1d2
Jfontaine <jfontaine19@gmail.com>
2022-07-04 13:58:08 -0400

    Created state for the pokemon and used axios to to fetch api data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import PokeList from "./PokeList";
 
 function App() {
-  return (
-    <div className="App">
+const [pokemon, setPokemon] = useState([])
 
-    </div>
-  );
+  axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+    console.log(res.data.results);
+    console.log(res.data.results.map(p => p.name));
+    setPokemon(res.data.results.map(p => p.name));
+  })
+
+  return <div className="App">
+    <PokeList pokemon={pokemon} />
+  </div>;
 }
 
 export default App;
