0d7a86c827303755a972b2d364ac7747f2164bf7
Jfontaine <jfontaine19@gmail.com>
2022-07-04 14:02:52 -0400

    Added the axios code to a useEffect. set the page as a parameter for the useEffect function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import PokeList from "./PokeList";
 
 function App() {
-const [pokemon, setPokemon] = useState([])
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState("https://pokeapi.co/api/v2/pokemon");
+  
+  useEffect(() => {
+    axios.get(currentPage).then(res => {
+      console.log(res.data.results);
+      console.log(res.data.results.map(p => p.name));
+      setPokemon(res.data.results.map(p => p.name));
+    })
+  }, [currentPage])
 
-  axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
-    console.log(res.data.results);
-    console.log(res.data.results.map(p => p.name));
-    setPokemon(res.data.results.map(p => p.name));
-  })
+ 
 
   return <div className="App">
     <PokeList pokemon={pokemon} />
